c893fffff0c625eff030bf3ef72d25dba652b39e
RJ Trujillo <eyecantcu@pm.me>
2022-06-05 21:36:09 -0600

    Add package handling to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
+# Copy over bashrc
 cp bashrc ~/.bashrc
 source ~/.bashrc
 
+# Deal with packages
+. remove.sh
+. install.sh
+
+# Copy over xbindkeys
 cp xbindkeysrc ~/.xbindkeysrc
 killall xbindkeys
 xbindkeys
