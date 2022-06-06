50d5b8623b138478658b0860b9ca3633caffc579
RJ Trujillo <eyecantcu@pm.me>
2022-06-05 21:32:25 -0600

    Add install script for packages
    Signed-off-by: RJ Trujillo <eyecantcu@pm.me>

---
+++ install.sh
@@ -0,0 +1,16 @@
+# NOTE: All of this will require interaction
+
+# First update
+sudo zypper dup
+
+# Now the rest
+sudo zypper in calibre opi xbindkeys
+opi codecs
+opi brave
+
+# Nvidia driver
+sudo zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
+sudo zypper in x11-video-nvidiaG06 x11-video-nvidiaG06-32bit nvidia-glG06 nvidia-glG06-32bit nvidia-computeG06 nvidia-computeG06-32bit libnvidia-egl-wayland1 libnvidia-egl-wayland1-32bit
+
+# Steam
+sudo zypper in steam
