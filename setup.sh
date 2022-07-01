ca4497714d8714ecc1b0c437071406470cbdb70c
RJ Trujillo <eyecantcu@pm.me>
2022-07-01 09:56:08 -0600

    Add Flathub remote for flatpaks
    Add to active user instead of system

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 cp bashrc ~/.bashrc
 source ~/.bashrc
 
+# Add Flathub remote (user)
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 # Deal with packages
 . remove.sh
 . install.sh
