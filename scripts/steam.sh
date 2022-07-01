32a4c2da56d83a31483b4a0a321f8ddb8b055294
RJ Trujillo <eyecantcu@pm.me>
2022-07-01 17:50:39 -0600

    Install Proton GE alongside protonup
    Produces the best compatability with native Windows game.

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -1,2 +1,13 @@
 # Steam
 sudo zypper in steam
+
+# Proton
+echo "Before setting up Proton GE, you must go through Steam's initial setup."
+while true; do
+    read -p "Have you ran Steam? " yn
+    case $yn in
+        [Yy]* ) mkdir ~/.steam/root/compatibilitytools.d/ && pip3 install protonup && protonup -d "~/.steam/root/compatibilitytools.d/" && protonup; break;;
+        [Nn]* ) break;;
+        * ) echo "Invalid option. Input yes (y) or no (n).";;
+    esac
+done
