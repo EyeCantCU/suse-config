75c370d8efa1e54ebc07aa83e58721684b48cc05
RJ Trujillo <eyecantcu@pm.me>
2022-07-01 18:30:30 -0600

    Replace protonup with protonup-ng
    protonup is currently borked.

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -6,7 +6,7 @@ echo "Before setting up Proton GE, you must go through Steam's initial setup."
 while true; do
     read -p "Have you ran Steam? " yn
     case $yn in
-        [Yy]* ) mkdir ~/.steam/root/compatibilitytools.d/ && pip3 install protonup && protonup -d "~/.steam/root/compatibilitytools.d/" && protonup; break;;
+        [Yy]* ) mkdir ~/.steam/root/compatibilitytools.d/ && pip3 install protonup-ng && protonup -d "~/.steam/root/compatibilitytools.d/" && protonup; break;;
         [Nn]* ) break;;
         * ) echo "Invalid option. Input yes (y) or no (n).";;
     esac
