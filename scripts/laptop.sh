10bccee4d4160c1f70c4dcf5b6c0ed3541ed838c
RJ Trujillo <eyecantcu@pm.me>
2022-07-02 23:41:04 -0600

    Don't blacklist the Nvidia driver
    Since prime offload is being used, there's no reason to blacklist
    the driver. Without manual intervention this prevents the Nvidia
    driver from being loaded which we don't want.

--- scripts/laptop.sh
+++ scripts/laptop.sh
@@ -6,6 +6,7 @@ sudo zypper in sof-firmware
 
 # Disable 1650 Ti when not in use
 echo Disabling Nvidia GPU when not utilized...
-cp ../rules/etc_udev_rules.d_80-nvidia-pm.rules /etc/udev/rules.d/80-nvidia-pm.rules
-cp ../conf/etc_modprobe.d_nvidia-pm.conf /etc/modprobe.d/nvidia-pm.conf
+sudo rm /lib/modprobe.d/09-nvidia-modprobe-bbswitch-G04.conf
+sudo cp ../rules/etc_udev_rules.d_80-nvidia-pm.rules /etc/udev/rules.d/80-nvidia-pm.rules
+sudo cp ../conf/etc_modprobe.d_nvidia-pm.conf /etc/modprobe.d/nvidia-pm.conf
 sudo systemctl enable nvidia-persistenced.service
