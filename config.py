9dc7a4232d70d7b9980137546634e1b0b1682ad5
RJ Trujillo <eyecantcu@pm.me>
2022-07-05 06:28:12 -0600

    Address an indentation problem
    Tabs to spaces
    
    Even with tabsize set to 4, and both VS Code and nano configured that
    way, everything else still suggests that the tabsize is 8. A tabsize
    of 8 in a Python script just looks absolutely ridiculous so let's just
    convert everything to spaces instead.

--- config.py
+++ config.py
@@ -5,39 +5,39 @@ import os
 import shutil
 
 def config(cwd, password, user):
-	conf_dir = cwd + '/' + input("In which directory is your configuration stored? ") + '/'
+    conf_dir = cwd + '/' + input("In which directory is your configuration stored? ") + '/'
 
-	print ("Setting up your configuration...")
-	for conf in os.scandir(conf_dir):
-		dest = conf.name.replace('_', '/')
-		if dest.startswith('root'):
-			dest = dest.removeprefix('root')
-			copy_conf = 'sudo -S cp ' + conf_dir + conf.name + ' ' + dest
-			copy = Popen(
-				copy_conf.split(),
-				stdin=PIPE, stdout=PIPE,
-				stderr=PIPE
-			)
-			copy.communicate(password.encode())
-			print("Copied " + conf.name + " to " + dest)
-		else:
-			dest = dest = "/home/" + user + "/"
-			if conf.name.endswith('rc'):
-				dest += "." + conf.name
-			else:
-				dest += conf.name
-			shutil.copyfile(conf, dest)
-			print("Copied " + conf.name + " to " + dest)
+    print ("Setting up your configuration...")
+    for conf in os.scandir(conf_dir):
+        dest = conf.name.replace('_', '/')
+        if dest.startswith('root'):
+            dest = dest.removeprefix('root')
+            copy_conf = 'sudo -S cp ' + conf_dir + conf.name + ' ' + dest
+            copy = Popen(
+                copy_conf.split(),
+                stdin=PIPE, stdout=PIPE,
+                stderr=PIPE
+            )
+            copy.communicate(password.encode())
+            print("Copied " + conf.name + " to " + dest)
+        else:
+            dest = dest = "/home/" + user + "/"
+            if conf.name.endswith('rc'):
+                dest += "." + conf.name
+            else:
+                dest += conf.name
+            shutil.copyfile(conf, dest)
+            print("Copied " + conf.name + " to " + dest)
 
-	add_config = input("Would you like to perform additional configuration? (y/N) ")
-	if add_config == 'y' or add_config == 'Y':
-		config(cwd, password, user)
+    add_config = input("Would you like to perform additional configuration? (y/N) ")
+    if add_config == 'y' or add_config == 'Y':
+        config(cwd, password, user)
 
 def main():
-	cwd = os.getcwd()
-	user = os.getlogin()
-	password = getpass("Enter your password (sudo): ")
+    cwd = os.getcwd()
+    user = os.getlogin()
+    password = getpass("Enter your password (sudo): ")
 
-	config(cwd, password, user)
+    config(cwd, password, user)
 
 main()
--- packages.py
+++ packages.py
@@ -5,107 +5,107 @@ import os
 import subprocess
 
 def elevated_cmd(cmd, password):
-	run = Popen(
-		cmd.split(),
-		stdout=PIPE, stdin=PIPE,
-		stderr=PIPE
-	)
-	run.communicate(password.encode())
+    run = Popen(
+        cmd.split(),
+        stdout=PIPE, stdin=PIPE,
+        stderr=PIPE
+    )
+    run.communicate(password.encode())
 
 def get_packages(file):
-	cwd = os.getcwd() + '/'
-	file = cwd + file
+    cwd = os.getcwd() + '/'
+    file = cwd + file
 
-	packagestxt = open(file, 'r')
-	packages = packagestxt.readlines()
-	package_list = ''
+    packagestxt = open(file, 'r')
+    packages = packagestxt.readlines()
+    package_list = ''
 
-	for package in packages:
-		package_list += package + ' '
-	return package_list
+    for package in packages:
+        package_list += package + ' '
+    return package_list
 
 def zypper(password):
-	ar_zypp = 'sudo -S zypper --non-interactive --quiet ar -C ' # link name
-	ref_zypp = 'sudo -S zypper --gpg-auto-import-keys ref'
-	in_zypp = 'sudo -S zypper in -y --auto-agree-with-licenses '
-	rm_zypp = 'sudo -S zypper rm -y '
+    ar_zypp = 'sudo -S zypper --non-interactive --quiet ar -C ' # link name
+    ref_zypp = 'sudo -S zypper --gpg-auto-import-keys ref'
+    in_zypp = 'sudo -S zypper in -y --auto-agree-with-licenses '
+    rm_zypp = 'sudo -S zypper rm -y '
 
-	while True:
-		print("Zypper Options:\n1. Import repos\n2. Install packages\n3. Remove packages\n4. Exit\n")
-		opt = int(input("Enter an option: "))
-		match opt:
-			case 1:
-				print("Adding repos...\n")
-				repostxt = open('repos.txt', 'r')
-				repos = repostxt.readlines()
-				for repo in repos:
-					ar = ar_zypp + repo
-					elevated_cmd(ar, password)
-					elevated_cmd(ref_zypp, password)
-			case 2:
-				print("Installing packages... (may take a while)\n")
-				in_zypp += get_packages('zypper.txt')
-				elevated_cmd(in_zypp, password)
-			case 3:
-				print("Removing packages...")
-				rm_zypp += get_packages('remove.txt')
-				elevated_cmd(rm_zypp, password)
-			case 4:
-				print("Returning to menu\n")
-				return
-			case _:
-				print("Invalid option. Please enter 1 to 4")
-				continue
+    while True:
+        print("Zypper Options:\n1. Import repos\n2. Install packages\n3. Remove packages\n4. Exit\n")
+        opt = int(input("Enter an option: "))
+        match opt:
+            case 1:
+                print("Adding repos...\n")
+                repostxt = open('repos.txt', 'r')
+                repos = repostxt.readlines()
+                for repo in repos:
+                    ar = ar_zypp + repo
+                    elevated_cmd(ar, password)
+                    elevated_cmd(ref_zypp, password)
+            case 2:
+                print("Installing packages... (may take a while)\n")
+                in_zypp += get_packages('zypper.txt')
+                elevated_cmd(in_zypp, password)
+            case 3:
+                print("Removing packages...")
+                rm_zypp += get_packages('remove.txt')
+                elevated_cmd(rm_zypp, password)
+            case 4:
+                print("Returning to menu\n")
+                return
+            case _:
+                print("Invalid option. Please enter 1 to 4")
+                continue
 
 def opi(password):
-	opt = input("Install packages? (y/N) ")
-	if opt == 'y' or opt == 'Y':
-		print("Installing packages... (may take a while)\n")
-		in_opi = 'opi '
-		in_opi += get_packages('opi.txt')
-		elevated_cmd(in_opi, password)
-	else:
-		print("Returning to menu\n")
+    opt = input("Install packages? (y/N) ")
+    if opt == 'y' or opt == 'Y':
+        print("Installing packages... (may take a while)\n")
+        in_opi = 'opi '
+        in_opi += get_packages('opi.txt')
+        elevated_cmd(in_opi, password)
+    else:
+        print("Returning to menu\n")
 
 def flatpak():
-	while True:
-		print("Flatpak Configuration:\n1. Add remote\n2. Install packages\n3. Exit\n")
-		opt = int(input("Enter an option: "))
-		match opt:
-			case 1:
-				add_remote = "flatpak remote-add --user --if-not-exists "
-				remotestxt = open('remotes.txt', 'r')
-				remotes = remotestxt.readlines()
-				for remote in remotes:
-					install_remote = add_remote + remote
-					subprocess.run(install_remote.split())
-			case 2:
-				fp_install = 'flatpak install ' + get_packages('flatpaks.txt')
-				subprocess.run(fp_install.split())
-			case 3:
-				return
-			case _:
-				print("Invalid option. Please enter 1 to 3\n")
-				continue
+    while True:
+        print("Flatpak Configuration:\n1. Add remote\n2. Install packages\n3. Exit\n")
+        opt = int(input("Enter an option: "))
+        match opt:
+            case 1:
+                add_remote = "flatpak remote-add --user --if-not-exists "
+                remotestxt = open('remotes.txt', 'r')
+                remotes = remotestxt.readlines()
+                for remote in remotes:
+                    install_remote = add_remote + remote
+                    subprocess.run(install_remote.split())
+            case 2:
+                fp_install = 'flatpak install ' + get_packages('flatpaks.txt')
+                subprocess.run(fp_install.split())
+            case 3:
+                return
+            case _:
+                print("Invalid option. Please enter 1 to 3\n")
+                continue
 
 def main():
-	password = getpass("Enter your password (sudo): ")
+    password = getpass("Enter your password (sudo): ")
 
-	while True:
-		print("Package Management:\n1. Zypper\n2. OPI\n3. Flatpak\n4. Exit\n")
-		opt = int(input("Enter an option: "))
-		match opt:
-			case 1:
-				zypper(password)
-			case 2:
-				opi(password)
-			case 3:
-				flatpak()
-			case 4:
-				print("Exiting...\n")
-				return
-			case _:
-				print("Invalid option. Please enter 1 to 4\n")
-				continue
+    while True:
+        print("Package Management:\n1. Zypper\n2. OPI\n3. Flatpak\n4. Exit\n")
+        opt = int(input("Enter an option: "))
+        match opt:
+            case 1:
+                zypper(password)
+            case 2:
+                opi(password)
+            case 3:
+                flatpak()
+            case 4:
+                print("Exiting...\n")
+                return
+            case _:
+                print("Invalid option. Please enter 1 to 4\n")
+                continue
 
 main()
